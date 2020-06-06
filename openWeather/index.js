81fd83275d7635e662c6b1088b6fe178cf0f73f6
donald vallejo <donny.vallejo@gmail.com>
2020-06-06 14:13:40 -0700

    created smaller json response

--- openWeather/index.js
+++ openWeather/index.js
@@ -1,19 +1,18 @@
 let request = require('request');
 
 let apiKey = '258164417d5e03c9b4c03804d050b426';
+let request = require('request');
+
+let apiKey = '***********************************';
 let city = 'portland';
-let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`
+let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`
 
-let weather = JSON.parse(body)
-let message = `It's ${weather.main.temp} degrees in
-               ${weather.name}!`;
-               console.log(message);
-
-               
 request(url, function (err, response, body) {
   if(err){
     console.log('error:', error);
   } else {
-    console.log('body:', body);
+    let weather = JSON.parse(body)
+    let message = `It's ${weather.main.temp} degrees in ${weather.name}!`;
+    console.log(message);
   }
 });
