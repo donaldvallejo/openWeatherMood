22adf3de6bb6d0e9b4a786bab4acbed88ef002cd
donald vallejo <donny.vallejo@gmail.com>
2020-06-06 17:57:03 -0700

    added thrid try

---
+++ 3openWeather/weather_app/src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>React Weather App</h1>
+      </header>
+      <main>
+        {/* add weather fetching component */}
+      </main>
+      <footer>
+        Page created by yournamehere
+      </footer>
+    </div>
+  );
+}
+
+export default App;
